92fd3ace7618f98162aefc7351df093340912f23
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-25 13:42:20 +0200

    feat: replace redirect with Navigate for session-based routing

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, redirect } from '@tanstack/react-router'
+import { createFileRoute, Navigate, redirect } from '@tanstack/react-router'
 import { useSession } from '../stores/useSessionStore'
 
 export const Route = createFileRoute('/')({
@@ -10,12 +10,8 @@ function RouteComponent() {
   const session = useSession((state) => state.session);
 
   // Redirect the user to the login page (if they are not signed in)
-  if(!session) return redirect({
-    to: "/artisan/auth/login"
-  })
+  if(!session) return <Navigate to="/artisan/auth/login" />
 
   // Redirect to the dashboard
-  return redirect({
-    to: "/artisan/dashboard"
-  })
+  return <Navigate to="/artisan/dashboard" />
 }
