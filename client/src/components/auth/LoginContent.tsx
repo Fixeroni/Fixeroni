13f1f4c70d6eecaa1322590077d8592acb0b8755
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-25 14:53:56 +0200

    feat: update LoginContent component to include Google login button and layout

--- client/src/components/auth/LoginContent.tsx
+++ client/src/components/auth/LoginContent.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function LoginContent() {
-  return <h2>Login</h2>;
+  return (
+    <article>
+      <button type="button" className="flex gap-4 text-black items-center justify-center w-full bg-white rounded-xl shadow-md px-8 py-2">
+        <img
+          src="/images/icons/google.png"
+          className="w-6 h-6"
+        />
+        Login in with Google
+      </button>
+
+      <article className="flex gap-2 items-center justify-center"></article>
+    </article>
+  )
 }
 
 export default LoginContent;
