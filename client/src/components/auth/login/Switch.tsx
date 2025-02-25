e7f2806d52a45be88ea51f8b0d89fe131c8c78d1
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-25 14:32:22 +0200

    feat: enhance Switch component layout for better user interaction between login and registration views

--- client/src/components/auth/login/Switch.tsx
+++ client/src/components/auth/login/Switch.tsx
@@ -10,7 +10,22 @@ function Switch({
   // Track the selected content;
   const [content, setContent] = useState<"login" | "register">("login");
 
-  return content === "login" ? login : register;
+  return (
+    <article className="w-full flex flex-col justify-center items-center">
+      <article className="flex gap-4 items-center mb-4 w-full justify-center items-center">
+        <article className={`${content === "login" && "border-b-2 border-primary"} cursor-pointer p-2 text-lg`} onClick={() => setContent("login")}>
+          Login
+        </article>
+        <article className={`${content === "register" && "border-b-2 border-primary"} cursor-pointer p-2 text-lg`} onClick={() => setContent("register")}>
+          Register
+        </article>
+      </article>
+
+      {
+        content === "login" ? login : register
+      }
+    </article>
+  )
 }
 
 export default Switch;
