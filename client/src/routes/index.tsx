18e8126b3390d1bb8215ef02f9876ec7f47cf13c
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-25 13:16:03 +0200

    feat: implement session-based redirection in RouteComponent

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,15 +1,21 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from '@tanstack/react-router'
+import { useSession } from '../stores/useSessionStore'
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  
+  // Get the session data
+  const session = useSession((state) => state.session);
 
-  return (
-    <article>
-    
-    </article>
-  )
+  // Redirect the user to the login page (if they are not signed in)
+  if(!session) return redirect({
+    to: "/artisan/auth/login"
+  })
+
+  // Redirect to the dashboard
+  return redirect({
+    to: "/artisan/dashboard"
+  })
 }
