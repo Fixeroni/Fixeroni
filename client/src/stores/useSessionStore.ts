fb69fd384d4d3ba4bf84db7b76d121e6ec923e0f
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-25 13:12:16 +0200

    feat: add types for State and Action in useSessionStore

--- client/src/stores/useSessionStore.ts
+++ client/src/stores/useSessionStore.ts
@@ -1,7 +1,15 @@
 import { create } from "zustand";
 import { Session } from "../types";
 
-export const useSession = create((set) => ({
+type State = {
+    session: null | Session
+}
+
+type Action = {
+    login: (session: Session) => void
+}
+
+export const useSession = create<State & Action>((set) => ({
     session: null,
     login: (session: Session) => set(() => ({ session })),
     logout: () => set(() => ({ session: null })),
