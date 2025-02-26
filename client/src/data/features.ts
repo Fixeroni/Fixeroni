51d1feb1bec8f8a2454d6747fbaba3770cfcf8f1
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-26 04:25:59 +0200

    feat: add feature images and data for showcasing services

---
+++ client/src/data/features.ts
@@ -0,0 +1,20 @@
+import { Feature } from "../types";
+
+const features: Feature[] = [
+    {
+        image: "/images/assets/feature-1.png",
+        title: "Find the right fix, Fast !",
+        description: "Need a pro? Fixeroni connects you with skilled artisans for any job big or small. Post your task, get offers, and hire with confidence.",
+        ctaText: "Find an Artisan",
+        side: "right"
+    },
+    {
+        image: "/images/assets/feature-2.png",
+        title: "Show Your Skills. Get More Gigs.",
+        description: "Join Fixeroni and connect with clients who need your expertise. Whether you're a plumber, carpenter, designer, or tech guru, get hired, get paid, and grow your business.",
+        ctaText: "Join as an Artisan",
+        side: "left"
+    }
+]
+
+export default features;
