fb022aeb9bf6f79e5895291ff3e9ca91cca21a6f
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-26 04:29:58 +0200

    feat: update Feature component to use 'cta' prop and enhance styling

--- client/src/components/auth/register/Feature.tsx
+++ client/src/components/auth/register/Feature.tsx
@@ -1,16 +1,16 @@
 import { Link } from "@tanstack/react-router"
-import { Feature } from "../../../types"
+import { Feature as FeatureType } from "../../../types"
 
-function Feature({image, title, description, side, ctaText}: Feature) {
+function Feature({image, title,cta, description, side, ctaText}: FeatureType) {
   return (
-    <article className={` ${side === "right" ? "flex-row-reverse" : ""} flex gap-4 items-center justify-center text-[#535353] shadow-xl bg-white rounded-xl`}>
-      <img src={image} />
+    <article className={` ${side === "right" ? "flex-row-reverse" : ""} flex gap-4 items-center justify-center text-[#535353] shadow-xl bg-white rounded-2xl p-4`}>
+      <img src={image} className="w-60 h-60" />
 
-      <article className="flex flex-col gap-2 items-center">
-        <h2 className="text-xl font-semibold">{title}</h2>
+      <article className="flex flex-col gap-2 justify-center md:max-w-[400px] px-4">
+        <h2 className="text-2xl font-bold">{title}</h2>
         <p className="text-md">{description}</p>
 
-        <Link to="/artisan/auth/register" className="text-white bg-primary rounded-xl p-2">
+        <Link to={cta} className="text-white bg-primary rounded-xl py-2 px-4 w-fit">
             {ctaText}
         </Link>
       </article>
