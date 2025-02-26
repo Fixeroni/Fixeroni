897b2a128b6b0e2949f930e30a1483338a8e99af
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-26 04:00:42 +0200

    feat: adjust font size of welcome message in LoginContent

--- client/src/routes/artisan/auth/login/index.tsx
+++ client/src/routes/artisan/auth/login/index.tsx
@@ -19,7 +19,7 @@ function RouteComponent() {
         {/* Section text */}
         <article className="flex flex-col gap-2 justify-center items-center text-center">
           <h2 className="text-2xl font-medium">Welcome to Fixeroni</h2>
-          <p className="text-gray-secondary text-lg">
+          <p className="text-gray-secondary text-md">
             Let us get things running smoothly and <br /> keep the world in
             working order.
           </p>
