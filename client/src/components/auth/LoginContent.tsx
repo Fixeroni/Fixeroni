2f1318f0fcaf9bb5ca39249124c4ddeb7a40a37f
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-26 03:55:48 +0200

    feat: enhance layout and responsiveness of LoginContent and Input components

--- client/src/components/auth/LoginContent.tsx
+++ client/src/components/auth/LoginContent.tsx
@@ -8,10 +8,10 @@ function LoginContent() {
   const [showPassword, setShowPassword] = useState<boolean>(false);
 
   return (
-    <article>
+    <article className="flex flex-col gap-4 items-center justify-center">
       <button
         type="button"
-        className="flex gap-4 text-black items-center justify-center w-full bg-white rounded-xl shadow-md px-8 py-2"
+        className="flex gap-4 text-black items-center justify-center w-full bg-white rounded-xl shadow-md px-8 py-2 md:max-w-[400px]"
       >
         <img src="/images/icons/google.png" className="w-6 h-6" />
         Login in with Google
@@ -23,7 +23,7 @@ function LoginContent() {
         <article className="w-[75px] h-[1px] bg-gray-primary"></article>
       </article>
 
-      <form className="flex flex-col gap-8">
+      <form className="flex flex-col gap-8 justify-center items-center">
         <Input
           type="email"
           startContent={<Mail className="text-primary-light" size={30} strokeWidth={2} />}
@@ -40,9 +40,13 @@ function LoginContent() {
           Forgot password ?
         </Link>
 
-        <button className="font-semibold text-white bg-primary shadow-sm hover:shadow-md transition duration-300 p-2 hover:cursor-pointer rounded-lg" type="submit">
+        <button className="font-semibold text-white bg-primary shadow-sm hover:shadow-md transition duration-300 p-2 hover:cursor-pointer rounded-lg md:min-w-[400px] md:max-w-[400px]" type="submit">
           Login
         </button>
+
+        <article className="flex gap-2 items-center text-center justify-center">
+          By signing up, you agree to the Fixeroni <Link to="/" className="text-primary underline">Terms of Service</Link> and <Link to="/" className="text-primary underline">Privacy Policy</Link>
+        </article>
       </form>
     </article>
   );
