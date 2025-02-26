a42a3ac4740aecde634a39a9fd004ff00e91aeed
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-26 04:30:08 +0200

    feat: update registration components to display features and adjust layout

--- client/src/data/features.ts
+++ client/src/data/features.ts
@@ -6,14 +6,14 @@ const features: Feature[] = [
         title: "Find the right fix, Fast !",
         description: "Need a pro? Fixeroni connects you with skilled artisans for any job big or small. Post your task, get offers, and hire with confidence.",
         ctaText: "Find an Artisan",
-        side: "right"
+        side: "left"
     },
     {
         image: "/images/assets/feature-2.png",
         title: "Show Your Skills. Get More Gigs.",
         description: "Join Fixeroni and connect with clients who need your expertise. Whether you're a plumber, carpenter, designer, or tech guru, get hired, get paid, and grow your business.",
         ctaText: "Join as an Artisan",
-        side: "left"
+        side: "right"
     }
 ]
 
--- client/src/routes/artisan/auth/register/index.tsx
+++ client/src/routes/artisan/auth/register/index.tsx
@@ -1,6 +1,9 @@
 import { createFileRoute } from '@tanstack/react-router'
 import AuthLayout from '../../../../components/shared/AuthLayout'
 import AuthHoverCard from '../../../../components/shared/AuthHoverCard'
+import features from '../../../../data/features'
+import Feature from '../../../../components/auth/register/Feature'
+import type { Feature as FeatureType } from '../../../../types'
 
 export const Route = createFileRoute('/artisan/auth/register/')({
   component: RouteComponent,
@@ -11,7 +14,11 @@ function RouteComponent() {
     <AuthLayout>
       <AuthHoverCard>
         <article className="flex flex-col gap-4 items-center justify-cente">
-          
+          {
+            features.map((feature: FeatureType, index: number) => (
+              <Feature key={index} {...feature} />
+            ))
+          }
         </article>
       </AuthHoverCard>
     </AuthLayout>
