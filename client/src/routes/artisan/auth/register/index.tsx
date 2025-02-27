c981273ec4835ec71cb3b3dba059efb077f36faa
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-27 09:36:36 +0200

    feat: implement registration form fields and enhance register component layout

--- client/src/routes/artisan/auth/register/index.tsx
+++ client/src/routes/artisan/auth/register/index.tsx
@@ -1,30 +1,45 @@
-import { createFileRoute } from "@tanstack/react-router";
-import AuthLayout from "../../../../components/shared/AuthLayout";
-import AuthHoverCard from "../../../../components/shared/AuthHoverCard";
-import features from "../../../../data/features";
-import Feature from "../../../../components/auth/register/Feature";
-import type { Feature as FeatureType } from "../../../../types";
+import { createFileRoute } from '@tanstack/react-router'
+import AuthLayout from '../../../../components/shared/AuthLayout'
+import AuthHoverCard from '../../../../components/shared/AuthHoverCard'
+import features from '../../../../data/features'
+import Feature from '../../../../components/auth/register/Feature'
+import type { Feature as FeatureType } from '../../../../types'
+import Switch from '../../../../components/auth/login/Switch'
+import RegisterContent from '../../../../components/auth/RegisterContent'
+import LoginContent from '../../../../components/auth/LoginContent'
 
-export const Route = createFileRoute("/artisan/auth/register/")({
+export const Route = createFileRoute('/artisan/auth/register/')({
   component: RouteComponent,
-});
+})
+
+function Register () {
+  
+
+  return (
+    <h2>Hello</h2>
+  )
+}
 
 function RouteComponent() {
-  return (
-    <AuthLayout>
-      <AuthHoverCard>
-        {/* Fixeroni icon */}
-        <img src="/images/branding/logo.png" className="w-40" />
 
-        {/* Section text */}
-        <article className="flex flex-col gap-2 justify-center items-center text-center">
-          <h2 className="text-2xl font-medium">Welcome to Fixeroni</h2>
-          <p className="text-gray-secondary text-md">
-            Let us get things running smoothly and <br /> keep the world in
-            working order.
-          </p>
-        </article>
-      </AuthHoverCard>
-    </AuthLayout>
-  );
+    return (
+      <AuthLayout>
+        <AuthHoverCard>
+          {/* Fixeroni icon */}
+          <img src="/images/branding/logo.png" className="w-40" />
+  
+          {/* Section text */}
+          <article className="flex flex-col gap-2 justify-center items-center text-center">
+            <h2 className="text-2xl font-medium">Welcome to Fixeroni</h2>
+            <p className="text-gray-secondary text-md">
+              Let us get things running smoothly and <br /> keep the world in
+              working order.
+            </p>
+          </article>
+  
+          {/* Login / Register switch */}
+          <Switch login={<LoginContent />} register={<Register />} />
+        </AuthHoverCard>
+      </AuthLayout>
+    );
 }
