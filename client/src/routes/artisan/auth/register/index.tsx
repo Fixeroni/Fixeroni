da8975cb68f0a8097efd5e6aa88f8a6b38188826
Kin Leon Zinzombe <kinzinzombe07@gmail.com>
2025-02-27 09:28:22 +0200

    [REMOVE]: Hover animation on auth hover card

--- client/src/routes/artisan/auth/register/index.tsx
+++ client/src/routes/artisan/auth/register/index.tsx
@@ -10,17 +10,22 @@ export const Route = createFileRoute('/artisan/auth/register/')({
 })
 
 function RouteComponent() {
-  return (
-    <AuthLayout>
-      <AuthHoverCard>
-        <article className="flex flex-col gap-4 items-center justify-cente">
-          {
-            features.map((feature: FeatureType, index: number) => (
-              <Feature key={index} {...feature} />
-            ))
-          }
-        </article>
-      </AuthHoverCard>
-    </AuthLayout>
-  )
+    return (
+      <AuthLayout>
+        <AuthHoverCard>
+          {/* Fixeroni icon */}
+          <img src="/images/branding/logo.png" className="w-40" />
+  
+          {/* Section text */}
+          <article className="flex flex-col gap-2 justify-center items-center text-center">
+            <h2 className="text-2xl font-medium">Welcome to Fixeroni</h2>
+            <p className="text-gray-secondary text-md">
+              Let us get things running smoothly and <br /> keep the world in
+              working order.
+            </p>
+          </article>
+  
+        </AuthHoverCard>
+      </AuthLayout>
+    );
 }
