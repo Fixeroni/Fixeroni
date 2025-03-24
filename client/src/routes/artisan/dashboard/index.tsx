276e38d1f8827c8c3bbc2ab0e822ae55d913583a
goodluck1455 <goodluckstephen237@gmail.com>
2025-03-24 10:07:40 +0100

    commit

--- client/src/routes/artisan/dashboard/index.tsx
+++ client/src/routes/artisan/dashboard/index.tsx
@@ -1,15 +1,21 @@
 import { createFileRoute } from "@tanstack/react-router";
 import DashboardMinPage from "./DashBoardComponent/DashboardMainPage";
-
-
+import DashBoardMap from "./DashBoardComponent/DashBoardMap";
 
 export const Route = createFileRoute("/artisan/dashboard/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>
-  <DashboardMinPage />
+  return (
+    <div className="flex overflow-hidden gap-5">
+      <div className="flex-grow-1">
+        <DashboardMinPage />
+      </div>
 
-  </div>
+      <div>
+        <DashBoardMap />
+      </div>
+    </div>
+  );
 }
